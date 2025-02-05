69317abd74b7d8d362793dd3260da5cf1b03be63
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-02-05 20:08:08 +0530

    Time: 1617 ms (9.98%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT 
-date_format(trans_date,'%Y-%m') as month    ,
+LEFT(trans_date, 7) AS month  ,
  country ,
 count(id) AS trans_count,
 sum(if(state = 'approved',1,0)) AS approved_count ,
