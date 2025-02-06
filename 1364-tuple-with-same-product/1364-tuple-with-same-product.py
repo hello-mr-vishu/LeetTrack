6141c9446ce864979b61ded3cbd72721e8cf7894
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-02-06 17:41:04 +0530

    Time: 437 ms (33.65%), Space: 46.4 MB (41.71%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -1,19 +1,20 @@
-from typing import List
 from collections import defaultdict
 
 class Solution:
-    def tupleSameProduct(self, nums: List[int]) -> int:
-        def nc2(n: int) -> int:
-            return (n * (n - 1)) // 2  
+    def nC2(self, n):
+        return (n * (n - 1)) // 2
 
-        ht = defaultdict(int)  
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                ht[nums[i] * nums[j]] += 1  
+    def tupleSameProduct(self, nums):
+        n = len(nums)
+        freq = defaultdict(int) 
 
-        cnt = 0
-        for key, values in ht.items():  
-            if values >= 2:
-                cnt += 8 * nc2(values)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                product = nums[i] * nums[j]
+                freq[product] += 1
 
-        return cnt
+        count = 0
+        for frequency in freq.values():
+            count += 8 * self.nC2(frequency)
+
+        return count
