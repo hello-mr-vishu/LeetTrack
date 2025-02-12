4cfd1d08da9db4a5d0de9a7509e298f14bade3dc
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-02-12 21:16:31 +0530

    Time: 3 ms (52.53%), Space: 17.9 MB (41.18%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        dic = {}
+        for ch in s:
+            dic[ch] = dic.get(ch, 0) + 1
+        for ch in t:
+            if dic.get(ch, 0) == 0:
+                return ch
+            else:
+                dic[ch] -= 1
