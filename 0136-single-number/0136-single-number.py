983ce51883c5adc0b6d533473bcb126c9739c876
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-02-26 12:30:33 +0530

    Time: 3 ms (66.01%), Space: 19.6 MB (77.77%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ht = {}
+        res = 0
         for i in range(len(nums)):
-          if nums[i] not in ht:
-            ht[nums[i]] = 1
-          else:
-            ht[nums[i]]+=1
-        for j in ht:
-          if ht[j] == 1:
-            return j
-        return None
+          res^=nums[i]
+        return res
