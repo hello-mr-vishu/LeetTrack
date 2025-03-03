9404230061bdf00542c47c6f9e479164ffed52c7
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-03-03 11:26:12 +0530

    Time: 4 ms (70.38%), Space: 19.6 MB (23.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -6,19 +6,28 @@ class Solution:
         # print(premap)
         for crs , pre in prerequisites:
           premap[crs].append(pre)
-        print(premap)
+        # print(premap)
         visitSet = set()
         def dfs(crs):
+          # print(visitSet,"1")
           if crs in visitSet:
+            # print("executed1")
             return False
           if premap[crs] == []:
+            # print(crs,"executed2")
             return True
+          # print("executed3")
           visitSet.add(crs)
+          # print(visitSet,"2")
           for pre in premap[crs]:
             if not dfs(pre): return False
           visitSet.remove(crs)
           premap[crs] = []
+          # print(premap)
           return True
         for crs in range(numCourses):
-          if not dfs(crs): return False
+          # print("***************",crs , "executed0")
+          if not dfs(crs): 
+            # print("executed4")
+            return False
         return True
