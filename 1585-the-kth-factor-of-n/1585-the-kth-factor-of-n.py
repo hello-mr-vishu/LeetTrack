9b7b4737ab93c52b059c71f399a7d8da3988976b
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-03-17 20:07:16 +0530

    Time: 0 ms (100%), Space: 17.9 MB (42.89%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
@@ -1,12 +1,8 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        f1, f2 = [], []
-        for s in range(1, int(sqrt(n)) + 1 ):
-            if n % s == 0:
-                f1 += [s]
-                f2 += [n//s]
-                
-        if f1[-1] == f2[-1]: f2.pop()
-            
-        factors = f1 + f2[::-1]
-        return -1 if len(factors) < k else factors[k-1]
+        for i in range(1, n+1):
+            if n % i == 0:
+                k -= 1
+                if k == 0:  
+                    return i
+        return -1
