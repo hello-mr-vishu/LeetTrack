4b8feda32017534446e8658e67665d250c288cae
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-03-20 22:36:30 +0530

    Time: 47 ms (7.04%), Space: 19.1 MB (93.98%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums):
+        prev = curr = 0
+        for value in nums:
+            prev, curr = curr, max(prev + value, curr)
+        return curr
+
+
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        points = [0] * 10001
+        for num in nums:
+            points[num] += num
+        return self.rob(points)
