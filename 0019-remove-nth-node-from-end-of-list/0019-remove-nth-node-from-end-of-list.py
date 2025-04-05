f9e76e39824acdc01e4cae233696178fd728f74d
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-04-05 10:20:41 +0530

    Time: 0 ms (100%), Space: 17.9 MB (34.64%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,25 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        cnt = 0
-        curr = head
-        while curr:
-            cnt +=1
-            curr = curr.next
-        
-        if n == cnt :
-            return head.next
-        cnt1 = cnt-n
-        # print(cnt,cnt1)
-        curr = head
-        # print(type(curr))
-        for _ in range(cnt - n - 1):
-            curr = curr.next
-        if curr.next:    
-            curr.next = curr.next.next
-        # curr.next.next = None
-        return head
+        # Create a dummy node to handle edge cases like removing the head
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = dummy
+
+        # Move fast n+1 steps ahead so the gap between slow and fast is n
+        for _ in range(n + 1):
+            fast = fast.next
+
+        # Move both fast and slow one step at a time until fast reaches the end
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        # Now, slow is just before the node to delete
+        slow.next = slow.next.next
+
+        return dummy.next
