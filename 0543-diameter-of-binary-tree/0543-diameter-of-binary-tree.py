14afb80d222b274fa6aeb25076ca2532b76412cb
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-05-09 20:35:02 +0530

    Time: 10 ms (12.68%), Space: 20.7 MB (95.34%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -22,13 +22,15 @@ class Solution:
     #     self.preOrder(dia+1,node.right)
     #     return dia
 
-        self.dia = 0 
-        def ht(node):
+        self.res = 0
+        def dfs(node):
             if not node:
                 return 0
-            left = ht(node.left)
-            right = ht(node.right)
-            self.dia = max(self.dia, left + right)
-            return 1 + max(left, right)
-        ht(root)
-        return self.dia
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            self.res = max(self.res,left+right)
+            return 1+max(left,right)
+
+        dfs(root)
+        return self.res
