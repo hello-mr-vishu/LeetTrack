19a97b49bcb410d0dc1bd4c043c38451bc594558
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-05-11 07:54:35 +0530

    Time: 0 ms (100%), Space: 18.2 MB (12.55%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -3,17 +3,14 @@ class Solution:
         if image[sr][sc] == color:
             return image        
         oldcolor = image[sr][sc]
-
-        def dfs(image,color,r,c,oldcolor):
-            if r<0 or r>=len(image) or c<0 or c>=len(image[0]) or image[r][c]!= oldcolor:
-                return
-            print(image[r][c])
-            image[r][c] = color
-            dfs(image,color,r-1,c,oldcolor)
-            dfs(image,color,r,c-1,oldcolor)
-            dfs(image,color,r+1,c,oldcolor)
-            dfs(image,color,r,c+1,oldcolor)
-
-
-        dfs(image,color,sr,sc,oldcolor)
+        self.dfs(image,color,sr,sc,oldcolor)
         return image
+    def dfs(self,image,color,r,c,oldcolor):
+        if r<0 or r>=len(image) or c<0 or c>=len(image[0]) or image[r][c]!= oldcolor:
+            return
+        print(image[r][c])
+        image[r][c] = color
+        self.dfs(image,color,r-1,c,oldcolor)
+        self.dfs(image,color,r,c-1,oldcolor)
+        self.dfs(image,color,r+1,c,oldcolor)
+        self.dfs(image,color,r,c+1,oldcolor)
