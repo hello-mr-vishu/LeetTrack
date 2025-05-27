6a5efdb9621b462db5d7d797e5403530605fb7c6
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-05-27 07:20:49 +0530

    Time: 0 ms (100%), Space: 18.1 MB (79.05%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,18 +1,51 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        # ------Binary Search-------
-        nums.sort()
         left = 0
-        right = len(nums)
-        while left<right:
+        right = len(nums)-1
+        
+        while left<=right:
             mid = (left+right)//2
-            if nums[mid]==target:
+            if nums[mid]== target:
                 return True
-            elif target>nums[mid]:
-                left = mid+1
+            if nums[left]==nums[mid]==nums[right]:
+                left = left+1
+                right = right-1
+            elif nums[left]<=nums[mid]:
+                if nums[left]<=target<nums[mid]:
+                    right = mid-1
+                else:
+                    left = mid+1
             else:
-                right = mid
+                if nums[mid]<target<=nums[right]:
+                    left = mid+1
+                else:
+                    right = mid-1
         return False
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # ------Binary Search-------
+        # nums.sort()
+        # left = 0
+        # right = len(nums)
+        # while left<right:
+        #     mid = (left+right)//2
+        #     if nums[mid]==target:
+        #         return True
+        #     elif target>nums[mid]:
+        #         left = mid+1
+        #     else:
+        #         right = mid
+        # return False
 
 
 
