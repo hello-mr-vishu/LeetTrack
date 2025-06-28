c2edbb512aa14d722af4f09fc4b185d9e8154b15
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-06-28 19:24:21 +0530

    Time: 0 ms (100%), Space: 17.7 MB (48.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,14 +4,23 @@ class Solution:
         # if n==0 or n==1:
         #     return 1
         # return self.climbStairs(n-1)+self.climbStairs(n-2)
-        memo = [-1]*(n+1)
-        def ways(n,memo):
-            if n==0 or n==1:
-                return 1
-            # if the result for this subproblem is 
-            # already computed then return it
-            if memo[n] != -1 :
-                return memo[n]
-            memo[n] = ways(n-1,memo)+ways(n-2,memo)
-            return memo[n]
-        return ways(n,memo)
+        
+        # Memoization
+        # memo = [-1]*(n+1)
+        # def ways(n,memo):
+        #     if n==0 or n==1:
+        #         return 1
+        #     # if the result for this subproblem is 
+        #     # already computed then return it
+        #     if memo[n] != -1 :
+        #         return memo[n]
+        #     memo[n] = ways(n-1,memo)+ways(n-2,memo)
+        #     return memo[n]
+        # return ways(n,memo)
+
+        one , two = 1,1
+        for i in range(n-1):
+            temp = one
+            one = one+two
+            two = temp
+        return one
