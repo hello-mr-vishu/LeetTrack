ed3e0d5a41322fb2691841c04f79c4295600ab45
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-07-06 15:25:12 +0530

    Time: 0 ms (100%), Space: 17.7 MB (54.38%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,34 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [-1]*(n+1)
+        if n== 0:
+            return 0
+        if n==1:
+            return nums[0]
+        dp = [0]*(n+1)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        # res = 0
+        for i in range(2,n):
+            pick = nums[i] + dp[i-2]   # (rob current + skip previous)
+            notpick = 0 + dp[i-1]
+            dp[i] = max(pick,notpick)
+        return dp[n-1]
 
-        def fun(idx):
-            if idx == 0:
-                return nums[0]
-            if idx < 0:
-                return 0
-            if dp[idx]!= -1:
-                return dp[idx]
-            pick = nums[idx]+ fun(idx-2)
-            notpick= 0 + fun(idx-1)
-            dp[idx] = max(pick,notpick)
-            return dp[idx]
-        return fun(len(nums)-1)
+        # n = len(nums)
+        # dp = [-1]*(n)
+
+        # def fun(idx):
+        #     if idx == 0:
+        #         return nums[0]
+        #     if idx < 0:
+        #         return 0
+        #     if dp[idx]!= -1:
+        #         return dp[idx]
+        #     pick = nums[idx]+ fun(idx-2)
+        #     notpick= 0 + fun(idx-1)
+        #     dp[idx] = max(pick,notpick)
+        #     print(dp)
+        #     return dp[idx]
+
+        # return fun(len(nums)-1)
