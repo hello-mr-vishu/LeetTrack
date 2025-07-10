fe22022cd229b87b1d36148bb86292bf5f046740
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-07-10 13:06:28 +0530

    Time: 1 ms (35.34%), Space: 17.8 MB (70.62%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -2,21 +2,35 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
-        def noofways(i,j):
-            if i<0 or j<0:
-                return 0            
-            if obstacleGrid[i][j] == 1:
-                return 0                
-            if i == 0 and j== 0:
-                return 1
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):    
+                if obstacleGrid[i][j]==1:
+                    dp[i][j] = 0    
+                elif i==0 and j==0:
+                    dp[i][j]=1
+                else:    
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1]
+
+        return dp[m-1][n-1]
+
+
+        # Memo
+        # def noofways(i,j):
+        #     if i<0 or j<0:
+        #         return 0            
+        #     if obstacleGrid[i][j] == 1:
+        #         return 0                
+        #     if i == 0 and j== 0:
+        #         return 1
             
-            if dp[i][j]!= -1:
-                return dp[i][j]
+        #     if dp[i][j]!= -1:
+        #         return dp[i][j]
 
-            up = noofways(i-1,j)
-            right = noofways(i,j-1)
+        #     up = noofways(i-1,j)
+        #     right = noofways(i,j-1)
 
-            dp[i][j] = up+right
-            return dp[i][j]
-        return noofways(m-1,n-1)
+        #     dp[i][j] = up+right
+        #     return dp[m-1][n-1]
+        # return noofways(m-1,n-1)
