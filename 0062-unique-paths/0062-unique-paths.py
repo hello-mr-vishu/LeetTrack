fa2445e74f66d74ef63331a29ba85fa9a038b4ba
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-07-10 11:13:04 +0530

    Time: 4 ms (7.88%), Space: 17.8 MB (65.3%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,21 +1,33 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def noofways(i,j):
-            if i == 0 or j == 0:
-                return 1
-            if i<0 or j<0:
-                return 0
-            if dp[i][j] != -1:
-                return dp[i][j]
+        # Tabulation
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                if i==0 or j ==0:
+                    dp[i][j]=1
+                else:
+                    dp[i][j] =  dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+        
+        # Memo
+        # def noofways(i,j):
+        #     if i == 0 or j == 0:
+        #         return 1
+        #     if i<0 or j<0:
+        #         return 0
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
 
-            up = noofways(i-1,j)
-            left = noofways(i,j-1)
-            dp[i][j] = up + left
+        #     up = noofways(i-1,j)
+        #     left = noofways(i,j-1)
+        #     dp[i][j] = up + left
 
-            return dp[i][j]
+        #     return dp[i][j]
 
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
-        return noofways(m-1,n-1)
+        # dp = [[-1 for _ in range(n)] for _ in range(m)]
+        # return noofways(m-1,n-1)
 
         
         # Recursive 
