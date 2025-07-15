27bd26a0a30bd3322a5f142b210fbe1f60b49c35
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-07-15 20:05:18 +0530

    Time: 17 ms (42.42%), Space: 20.3 MB (40.21%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,21 +1,40 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        def minPath(r, c):
-            if r < 0 or c < 0:
-                return float('inf')
-            if r == 0 and c == 0:
-                return grid[0][0]
-            if dp[r][c] != -1:
-                return dp[r][c]
-            up = minPath(r - 1, c)
-            left = minPath(r, c - 1)
-            dp[r][c] = grid[r][c] + min(up, left)
-            return dp[r][c]
+        # Taulation
+        r = len(grid)
+        c = len(grid[0])
+        dp = [[0 for _ in range(c)] for _ in range(r)]
+    
+        for i in range(r):
+            for j in range(c):
+                 if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                 else:
+                    up = dp[i - 1][j] if i > 0 else float('inf')
+                    left = dp[i][j - 1] if j > 0 else float('inf')
+                    dp[i][j] = grid[i][j] + min(up, left)
+        return dp[r-1][c-1]
 
-        m = len(grid)
-        n = len(grid[0])
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
-        return minPath(m - 1, n - 1)
+
+
+
+        # Memoization
+        # def minPath(r, c):
+        #     if r < 0 or c < 0:
+        #         return float('inf')
+        #     if r == 0 and c == 0:
+        #         return grid[0][0]
+        #     if dp[r][c] != -1:
+        #         return dp[r][c]
+        #     up = minPath(r - 1, c)
+        #     left = minPath(r, c - 1)
+        #     dp[r][c] = grid[r][c] + min(up, left)
+        #     return dp[r][c]
+
+        # m = len(grid)
+        # n = len(grid[0])
+        # dp = [[-1 for _ in range(n)] for _ in range(m)]
+        # return minPath(m - 1, n - 1)
 
 
         # Recursive solution
