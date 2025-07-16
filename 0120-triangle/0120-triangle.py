bd0f4bbe4ea8926e00359f010a39575d9f678be4
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-07-16 20:50:04 +0530

    Time: 7 ms (36.36%), Space: 19.1 MB (55.53%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,14 +1,24 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        def rectotal(i,j):
-            
-            if i == len(triangle)-1:
-                return triangle[i][j]
-            if dp[i][j] != -1:
-                return dp[i][j]
-            dp[i][j] = triangle[i][j] + min(rectotal(i+1,j),rectotal(i+1,j+1))
-            return dp[i][j]
-        # dp = [[-1 for _ in range(len(triangle))] for _ in range(len(triangle))]
+        # tabulation
         n = len(triangle)
-        dp = [[-1 for _ in range(len(triangle[row]))] for row in range(n)]
-        return rectotal(0,0)
+        for i in range(n-2,-1,-1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] +=  min(triangle[i+1][j],triangle[i+1][j+1])
+        return triangle[0][0]
+
+
+
+        # Memoization
+        # def rectotal(i,j):
+            
+        #     if i == len(triangle)-1:
+        #         return triangle[i][j]
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+        #     dp[i][j] = triangle[i][j] + min(rectotal(i+1,j),rectotal(i+1,j+1))
+        #     return dp[i][j]
+        # # dp = [[-1 for _ in range(len(triangle))] for _ in range(len(triangle))]
+        # n = len(triangle)
+        # dp = [[-1 for _ in range(len(triangle[row]))] for row in range(n)]
+        # return rectotal(0,0)
