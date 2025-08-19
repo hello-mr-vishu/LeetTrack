ae7096d1f15d3c5dc1cb238a1f930fe958c56571
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-08-19 10:36:39 +0530

    Time: 4 ms (5.85%), Space: 18.1 MB (50.79%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,37 +1,21 @@
-from collections import deque
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        # bfs approach
-        # if the image is large — Python’s default recursion limit might be exceeded. For large grids, consider using an iterative BFS approach.
-        rows , columns = len(image),len(image[0])
-        oldcolor = image[sr][sc]
-        if image[sr][sc]==color:
+        n = len(image)
+        m = len(image[0])
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        original = image[sr][sc]
+        if original == color:
             return image
-        q = deque([(sr,sc)])
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
-        image[sr][sc]= color
+        # image[sr][sc] = color
+        q = deque()
+        q.append((sr,sc))
         while q:
-            x,y = q.popleft()
-            for dx,dy in directions:
-                nx,ny = x+dx,y+dy
-
-                if 0<=nx<rows and 0<=ny<columns and image[nx][ny] == oldcolor:
-                    image[nx][ny] = color
-                    q.append((nx,ny))
+            i,j = q.popleft()
+            if image[i][j] == original:
+                image[i][j] = color
+                for dx,dy in directions:
+                    x = i + dx
+                    y = j + dy
+                    if 0 <= x < n and 0<= y <m and image[x][y] == original:
+                        q.append((x,y))
         return image
-
-    # dfs
-    #     if image[sr][sc] == color:
-    #         return image        
-    #     oldcolor = image[sr][sc]
-    #     self.dfs(image,color,sr,sc,oldcolor)
-    #     return image
-    # def dfs(self,image,color,r,c,oldcolor):
-    #     if r<0 or r>=len(image) or c<0 or c>=len(image[0]) or image[r][c]!= oldcolor:
-    #         return
-    #     print(image[r][c])
-    #     image[r][c] = color
-    #     self.dfs(image,color,r-1,c,oldcolor)
-    #     self.dfs(image,color,r,c-1,oldcolor)
-    #     self.dfs(image,color,r+1,c,oldcolor)
-    #     self.dfs(image,color,r,c+1,oldcolor)
