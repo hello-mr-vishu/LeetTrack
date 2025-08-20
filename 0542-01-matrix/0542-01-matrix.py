06d770c99303049fae5a09353831bc44782d6f5d
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-08-20 13:39:36 +0530

    Time: 132 ms (59.64%), Space: 20.7 MB (50.56%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,25 +1,24 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        rows = len(mat)
-        cols = len(mat[0])
-        res = [[float('inf')] * cols for _ in range(rows)]
-        for i in range(rows):
-            for j in range(cols):
-                res[i][j] = float('inf')
-                if mat[i][j] == 0 :
-                    res[i][j] = 0
-                else:
-                    if i>0:
-                        res[i][j] = min(res[i][j],res[i-1][j]+1)
-                    if j>0:
-                        res[i][j] = min(res[i][j],res[i][j-1]+1)
-
-        for i in range(rows-1,-1,-1):
-            for j in range(cols-1,-1,-1):
-                if mat[i][j] != 0:
-                    if  i< rows-1:
-                        res[i][j] = min(res[i][j],res[i+1][j]+1)
-                    if j<cols-1:
-                        res[i][j] = min(res[i][j],res[i][j+1]+1)
-
-        return res
+        n = len(mat)
+        m = len(mat[0])
+        ans = [[float('inf')]*m for _ in range(n)]
+        q = deque()
+        # print(ans)
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == 0:
+                    ans[i][j]=0
+                    q.append((i,j))
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        while q:
+            len_q = len(q)
+            x,y = q.popleft()
+            for dx, dy in directions:
+                i = x+dx
+                j = y+dy
+                if 0<=i<n and 0<=j<m :
+                    if ans[i][j] > ans[x][y]+1:
+                        ans[i][j] = ans[x][y] + 1
+                        q.append((i,j))
+        return ans
