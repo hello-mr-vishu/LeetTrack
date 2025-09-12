a2d8921da819cb7825cbdc92b75787b52886ac41
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-09-12 11:18:30 +0530

    Time: 0 ms (100%), Space: 17.7 MB (78.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -18,9 +18,21 @@ class Solution:
         #     return memo[n]
         # return ways(n,memo)
 
-        one , two = 1,1
-        for i in range(n-1):
-            temp = one
-            one = one+two
-            two = temp
-        return one
+        # one , two = 1,1
+        # for i in range(n-1):
+        #     temp = one
+        #     one = one+two
+        #     two = temp
+        # return one
+
+        def rec(n):
+            if n == 1 or n == 0:
+                return 1
+            if dp[n] != -1:
+                return dp[n]
+            dp[n] = rec(n-1) + rec(n-2)
+            return dp[n]
+        dp = [-1]*(n+1)
+        # for i in range(n):
+            # dp[i] == -1
+        return rec(n)
