24d149db9ed73a69298d65f46dd95699fac001d1
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-10-21 07:22:20 +0530

    Time: 3 ms (81.53%), Space: 17.6 MB (99.14%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,32 +1,34 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        # directions = [[1,0],[0,1],[-1,0],[0,-1]]
-        directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        def bfs(grid):
-            q = deque()
-            fresh = 0
-            for i in range(n):
-                for j in range(m):
-                    if grid[i][j] == 2:
-                        q.append((i,j))
-                    elif grid[i][j] ==1:
-                        fresh += 1
-            elapsedtime = 0
-
-            while q and fresh >0:
-                elapsedtime +=1
-                for _ in range(len(q)):
-                    i,j = q.popleft()
-
-                    for dir in directions:
-                        x = i +dir[0]
-                        y = j +dir[1]
-                        if 0<=x and x<n and y>=0 and y<m and grid[x][y] ==1:
-                            grid[x][y] = 2
-                            fresh -= 1
-                            q.append((x,y))
+        m = len(grid)
+        n = len(grid[0])
+        queue = deque()
+        fresh = 0
             
-            return elapsedtime if fresh ==0  else -1
-        return bfs(grid)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2 :
+                    queue.append((i,j))
+                elif grid[i][j] == 1:
+                    fresh +=1
+        if fresh == 0:
+            return 0
+        
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        time = -1
+        
+        while queue:
+            time += 1
+            for _ in range(len(queue)):
+                x,y = queue.popleft()
+                for dir in directions:
+                    i = x+dir[0]
+                    j = y+dir[1]
+                    if 0<=i<m and 0<=j<n and grid[i][j]==1:
+                        grid[i][j] = 2
+                        queue.append((i,j))
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    return -1
+        return time
