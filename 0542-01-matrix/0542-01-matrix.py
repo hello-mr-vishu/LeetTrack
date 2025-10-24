9ecb268d0188fa66bc28d70ba221815e4c61ab55
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-10-24 08:50:18 +0530

    Time: 125 ms (67.64%), Space: 19.6 MB (85.11%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,24 +1,22 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        n = len(mat)
-        m = len(mat[0])
-        ans = [[float('inf')]*m for _ in range(n)]
+        m = len(mat)
+        n = len(mat[0])
         q = deque()
-        # print(ans)
-        for i in range(n):
-            for j in range(m):
-                if mat[i][j] == 0:
-                    ans[i][j]=0
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j]== 0:
                     q.append((i,j))
-        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+                else:
+                    mat[i][j] = float('inf')
         while q:
-            len_q = len(q)
             x,y = q.popleft()
-            for dx, dy in directions:
+            for dx,dy in directions:
                 i = x+dx
                 j = y+dy
-                if 0<=i<n and 0<=j<m :
-                    if ans[i][j] > ans[x][y]+1:
-                        ans[i][j] = ans[x][y] + 1
+                if 0<=i<m and 0<=j<n :
+                    if mat[i][j] > mat[x][y]+1:
+                        mat[i][j] = mat[x][y]+1
                         q.append((i,j))
-        return ans
+        return mat
