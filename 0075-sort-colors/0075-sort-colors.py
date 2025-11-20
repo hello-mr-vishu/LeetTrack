ee865261b55baaa09747b77555ee7eeceb5979cc
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-11-20 11:13:13 +0530

    Time: 0 ms (100%), Space: 17.9 MB (33.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,22 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        freq = {0:0,1:0,2:1}
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                freq[0] += 1
-            elif nums[i] == 1:
-                freq[1] += 1
+        low = 0
+        mid = 0
+        high = len(nums)-1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low],nums[mid] = nums[mid],nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
             else:
-                freq[2] += 1
-        for i in range(len(nums)):
-            if freq[0]>0:
-                nums[i] = 0
-                freq[0] -= 1
-            elif freq[1]>0:
-                nums[i]=1
-                freq[1] -=1
-            else:
-                nums[i]=2
-                freq[2] -= 1
+                nums[high] , nums[mid] = nums[mid] , nums[high]
+                high -= 1
         return nums
